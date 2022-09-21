11223b8401db6ff4463ef0389dd6502b1b68e1b7
chanchal16 <rajputchanchal17@gmail.com>
2022-09-22 01:28:03 +0530

    update details

--- src/pages/SingleCharacter.jsx
+++ src/pages/SingleCharacter.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom'
+import { HousesChart } from '../components/HousesChart';
+import { SeasonChart } from '../components/SeasonChart';
 
 export const SingleCharacter = () => {
     const {cname} = useParams();
@@ -15,20 +17,25 @@ export const SingleCharacter = () => {
   return (
     <section>
         <h1 className='text-2xl'>hello</h1>
-        <div className='flex justify-between p-4'>
+        <div className='flex justify-between p-4 my-6'>
             <div className='w-24 border border-gray-500 m-4'>
             <img src={currCharacter.person.imgurl} alt={currCharacter.name} 
             className=''/>
             </div>
             <div className='m-4'>
-                <h2>Name: {currCharacter.name}</h2>
-                <h3>Titles hold: {currCharacter.titles.join(',')}</h3>
-                <p>House: {house[0].name}</p>
-                <p>Region: {house[0].region}</p>
-                <h4>Word: {house[0].words}</h4>
-                <h5>Coat of Arms: {house[0].coatOfArms}</h5>
-                <p>Seats hold: {house[0].seats.join(', ')}</p>
+                <h2>Name: {currCharacter?.name}</h2>
+                <h3>Titles hold: {currCharacter?.titles?.join(',')}</h3>
+                <p>House: {house?.name}</p>
+                <p>Region: {house?.region}</p>
+                <h4>Word: {house?.words}</h4>
+                <h5>Coat of Arms: {house?.coatOfArms}</h5>
+                <p>Seats hold: {house?.seats?.join(', ')}</p>
             </div>
+        </div>
+        {/* charts */}
+        <div className='flex justify-between gap-4'>
+            <SeasonChart currCharacter={currCharacter} />
+            <HousesChart currCharacter={currCharacter} /> 
         </div>
     </section>
   )
