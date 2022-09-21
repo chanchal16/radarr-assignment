1de4df817e79ca7dd97a55429354afd3f0701e4b
chanchal16 <rajputchanchal17@gmail.com>
2022-09-21 18:05:43 +0530

    chore - add route to single page

--- src/features/characters/CharacterList.jsx
+++ src/features/characters/CharacterList.jsx
@@ -3,13 +3,13 @@ import { useDispatch, useSelector } from 'react-redux'
 import { getCharacters, getHouse, SET_CHARACTERS } from './characterSlice';
 import axios from 'axios';
 import { CharactersData } from '../../CharactersData';
+import { useNavigate, Link } from "react-router-dom";
 
 export const CharacterList = () => {
+    const navigate = useNavigate()
     const characterState = useSelector(state=>state.characters)
     const{characterList,house} = characterState
     const dispatch = useDispatch()
-
-
 
     // const getAllCharacters = useCallback(
     //   () => {
@@ -39,10 +39,11 @@ export const CharacterList = () => {
     
     }, [])
 
-    const getInfo = (housename)=>{
+    const getInfo = (housename,cname)=>{
         console.log('hname',housename)
         dispatch(getHouse({housename:housename}))
         console.log('fetched',house)
+        navigate(`/characters/${cname}`)
     }
     
     
@@ -53,10 +54,10 @@ export const CharacterList = () => {
         {
             characterList.map((character,index)=>(
                 <div key={index} className='w-40 border border-solid border-zinc-700 p-4'>
-                    <img src={character.person.imgurl} alt={character.name} className='w-10' /> 
+                    <img src={character.person.imgurl} alt={character.name} className='w-20' /> 
                     <h3 className='text-xl'> {character.name}</h3>
                     <div className='flex'>
-                        <button onClick={()=>getInfo(character.person.house)}>info</button>
+                        <button onClick={()=>getInfo(character.person.house,character.name)}>info</button>
                         <button>play</button>
                     </div>
                 </div>
