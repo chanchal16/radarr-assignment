11717264c5a504dee582f6ce5556f5c75e834494
chanchal16 <rajputchanchal17@gmail.com>
2022-09-22 01:25:39 +0530

    Update - add routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Routes, Route } from "react-router-dom";
 import { CharacterList } from './features/characters/CharacterList'
 import { SingleCharacter } from './pages/SingleCharacter';
+import { CheckCharacterDetails } from './pages/CheckCharacterDetails';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<CharacterList/>} />
         <Route path='/characters/:cname' element={<SingleCharacter/>}/>
+        <Route path='/play/:name' element={<CheckCharacterDetails/>}/>
       </Routes>
       
     </div>
