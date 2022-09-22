a5f11aa5d8d59819834721ae83350db47021bbcf
chanchal16 <rajputchanchal17@gmail.com>
2022-09-22 12:05:12 +0530

    style - change font size of modal message

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export const Modal = () => {
   return (
     <div className='fixed top-0 left-0 z-10 w-full h-full scale-110 bg-[#00000080]'>
-        <div className='w-20 absolute top-1/2 left-1/2 m-auto bg-white transform-div'>
-            <h1 className='text-2xl'>Yayy!! you won</h1>
-            <small className='text-xl'>Woohoo!!🥳🎉</small>
+        <div className='w-80 absolute top-1/2 left-1/2 m-auto bg-white transform-div'>
+            <h1 className='text-xl my-1 text-gray-900'>Yayy!! you won, you are a true thronie⚔️</h1>
+            <small className='text-lg text-center my-1 text-orange-600'>Woohoo!!🥳🎉</small>
         </div>
     </div>
   )
