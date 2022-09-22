1fe2436d7d98bcb20b0f754f73ee2dcadbfdedb7
chanchal16 <rajputchanchal17@gmail.com>
2022-09-22 12:04:06 +0530

    Update - add house ids

--- src/CharactersData.js
+++ src/CharactersData.js
@@ -2,71 +2,71 @@ export const CharactersData = [
     {
         id:16,
         imgurl:'https://thronesapi.com/assets/images/margaery-tyrell.jpg',
-        house:'House Tyrell of Highgarden'
+        house: 398       //'House Tyrell of Highgarden'
     },
     {
         id:216,
         imgurl:'https://thronesapi.com/assets/images/brienne-tarth.jpeg',
-        house:'House Tarth of Evenfall Hall'
+        house:380  //'House Tarth of Evenfall Hall'
     },
     {
         id:238,
         imgurl:'https://thronesapi.com/assets/images/cersei.jpg',
-        house:'House Lannister of Casterly Rock'
+        house:229 //'House Lannister of Casterly Rock'
     },
     {
         id:271,
-        imgurl:'https://thronesapi.com/assets/images/daenerys.jpg',
-        house:"House Targaryen of King's Landing"
+        imgurl:'https://static.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/daenarys-1920.jpg?w=1200',
+        house:378  //"House Targaryen of King's Landing"
     },
     {
         id:583,
         imgurl:'https://thronesapi.com/assets/images/jon-snow.jpg',
-        house:'House Stark of Winterfell'
+        house: 362    //'House Stark of Winterfell'
     },
     {
         id:529,
         imgurl:'https://thronesapi.com/assets/images/jaime-lannister.jpg',
-        house:'House Lannister of Casterly Rock'
+        house: 229  //'House Lannister of Casterly Rock'
     },
     {
         id:957,
         imgurl:'https://thronesapi.com/assets/images/sansa-stark.jpeg',
-        house:'House Stark of Winterfell'
+        house:362 //'House Stark of Winterfell'
     },
     {
         id:1052,
-        imgurl:'https://thronesapi.com/assets/images/tyrion-lannister.jpg',
-        house:'House Lannister of Casterly Rock'
+        imgurl:'https://static.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/tyrion-lannister-1920.jpg?w=1200',
+        house:229   //'House Lannister of Casterly Rock'
     },
     {
         id:1022,
         imgurl:'https://thronesapi.com/assets/images/theon.jpg',
-        house:'House Greyjoy of Pyke'
+        house:169 //'House Greyjoy of Pyke'
     },
     {
         id:849,
         imgurl:'https://thronesapi.com/assets/images/ramsey-bolton.jpg',
-        house:'House Bolton of the Dreadfort'
+        house:34 //'House Bolton of the Dreadfort'
     },
     {
         id:565,
         imgurl:'https://thronesapi.com/assets/images/joffrey.jpg',
-        house:"House Baratheon of Storm's End"
+        house:17 //"House Baratheon of Storm's End"
     },
     {
         id:1319,
         imgurl:'https://thronesapi.com/assets/images/davos-seaworth.png',
-        house:'House Seaworth of Cape Wrath'
+        house:340 //'House Seaworth of Cape Wrath'
     },
     {
         id:954,
         imgurl:'https://thronesapi.com/assets/images/sam.jpg',
-        house:'House Tarly of Horn Hill'
+        house:379    //'House Tarly of Horn Hill'
     },
     {
         id:1560,
         imgurl:'https://thronesapi.com/assets/images/jorah-mormont.jpg',
-        house:'House Mormont of Bear Island'
+        house:271 //'House Mormont of Bear Island'
     }
 ]
